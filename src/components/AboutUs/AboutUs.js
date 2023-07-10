753c33419fcf7abd11a50b54755a43eb4fc97a18
Amaroke <hugo.amaroke@gmail.com>
2023-07-10 18:20:15 +0200

    fix typo

--- src/components/AboutUs/AboutUs.js
+++ src/components/AboutUs/AboutUs.js
@@ -7,7 +7,7 @@ const PartnersPage = () => {
 
     return (
         <div className="streams-page">
-            <p>Yurei Esports – <br /><br />Wir sind eine 2023 gegründete professionelle eSport-Organisation, die sich für<br/> das Wohlsein ihrer Spieler einsetzt und der Community versucht, Spaß am<br/> eSport zu bieten.<br /><br />Unser Fokus liegt auf das MOBA-Game „League of Legends“, in Zukunft sind<br/> aber auch andere Spiele geplant.</p>
+            <p>Yurei Esports<br /><br />Wir sind eine 2023 gegründete professionelle eSport-Organisation, die sich für<br/> das Wohlsein ihrer Spieler einsetzt und der Community versucht, Spaß am<br/> eSport zu bieten.<br /><br />Unser Fokus liegt auf das MOBA-Game „League of Legends“, in Zukunft sind<br/> aber auch andere Spiele geplant.</p>
         </div>
     );
 };
